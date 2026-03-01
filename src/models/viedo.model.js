3de16e2fabc02ec65b1797c3ef43c5cadcff9dff
Nishant Sharma <nishnatsharma5330@gmail.com>
2026-03-01 14:49:46 +0530

    add models

---
+++ src/models/viedo.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const viedoSchema = new Schema(
+    {
+        viedoFile: {
+            type: String,    //cloudinary url
+            required: true,
+
+        },
+
+        thumbnail: {
+            type: String,  //cloudinary url
+            required: true,
+        },
+
+        title: {
+            type: String,
+            required: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+        },
+
+        duration: {
+            type: Number,
+            required: true,
+        },
+
+        views: {
+            type: Number,
+            default: 0
+        },
+
+        isPublised: {
+            type: Boolean,
+            default: true
+        },
+
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+viedoSchema.plugin(mongooseAggregatePaginate)
+
+export const Viedo = mongoose.model("Viedo", viedoSchema)
